46bd32dd5afa7b7c5a0fc256b32ff450edb52c2e
propellerhead <lars.hundebol@gmail.com>
2014-03-15 18:01:39 +0100

    Remove commenting feature from blog.

--- main.py
+++ main.py
@@ -6,37 +6,10 @@ from google.appengine.ext.webapp import util
 
 from recaptcha.client import captcha
 
-#import helpers
 from helpers import *
 
-class MainHandler(webapp.RequestHandler):
-  def post(self):
-    path = self.request.path.split('/')
-    remoteip = self.request.remote_addr
-    url = self.request.url
-    
-    name = self.request.get('name')
-    email = self.request.get('email')
-    comment = self.request.get('comment')
-    challenge = self.request.get('recaptcha_challenge_field')
-    response  = self.request.get('recaptcha_response_field')
-    post_id = long(path[2])
-    
-    cResponse = captcha.submit(challenge, response, '6LdOAb4SAAAAALH1zbcxUkjwFYJQItNFHDd5zj_h', remoteip)
 
-    if cResponse.is_valid:
-      comment = BlogComment(
-        post_id = post_id,
-        name = name,
-        email = email,
-        comment = comment)
-      comment.put()
-      
-      self.redirect(url)
-    else:
-      error = 'The values were not valid.'
-      self.response.out.write('It failed with error code: ' + error);
-  
+class MainHandler(webapp.RequestHandler):  
   def get(self):
     twitter_id = '7942102'
   
@@ -61,15 +34,8 @@ class MainHandler(webapp.RequestHandler):
     else:
       page_id = 1
       blog = blogger.get_feeds(page_id, page_items)
-    
-    chtml = captcha.displayhtml(
-      public_key = "6LdOAb4SAAAAAEkajiU_r5ksvtPasK7EUAXKKtnD",
-      use_ssl = False,
-      error = None)
       
-    template_values = { 
-      'captchahtml': chtml,
-      'tweets': tweets,
+    template_values = {
       'blog': blog
     }
     
